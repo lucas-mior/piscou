15d731cff0ce33c1ef052f7a39c3c621a0f667a6
lucas-mior <lucas.mior@tutamail.com>
2022-08-23 18:47:32 -0300

    style

--- main.c
+++ main.c
@@ -15,21 +15,21 @@ int main(int argc, char *argv[]) {
     printf("Hello from %s!\n", argv[0]);
     int option;
     while((option = getopt(argc, argv, "hx:")) != -1){ //get option from the getopt() method
-          switch(option){
-             case 'h':
-                usage();
-             case 'f': //here f is used for some file name
-                printf("Given File: %s\n", optarg);
-                break;
-             case 'x': //here f is used for some file name
-                printf("Given File: %s\n", optarg);
-                break;
-             case ':':
-                printf("option needs a value\n");
-                break;
-             case '?': //used for some unknown options
-                printf("unknown option: %c\n", optopt);
-                break;
-          }
-       }
+      switch(option){
+         case 'h':
+            usage();
+         case 'f': //here f is used for some file name
+            printf("Given File: %s\n", optarg);
+            break;
+         case 'x': //here f is used for some file name
+            printf("Given File: %s\n", optarg);
+            break;
+         case ':':
+            printf("option needs a value\n");
+            break;
+         case '?': //used for some unknown options
+            printf("unknown option: %c\n", optopt);
+            break;
+      }
+   }
 }
