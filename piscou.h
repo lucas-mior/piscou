60c6efc9beb69255c07826af4f59c9bebd517db6
lucas-mior <lucas.mior@tutamail.com>
2022-08-24 18:36:50 -0300

    Command struct

--- piscou.h
+++ piscou.h
@@ -1,4 +1,15 @@
 #ifndef piscou_h
 #define piscou_h
 
+typedef struct Command {
+    char *mime;
+    char *args[10];
+} Command;
+
+Command commands[] = {
+    {.mime = "text/*",  .args = {"bat", "-p", "main.c"}},
+    {.mime = "image/*", .args = {"echo", "image", "%piscou-filename%"}},
+    {.mime = "video/*", .args = {"vid.sh", "%piscou-filename"}},
+};
+
 #endif /* piscou_h */
