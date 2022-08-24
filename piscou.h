0c511482c167fe808db035938035f7dee9d454cd
lucas-mior <lucas.mior@tutamail.com>
2022-08-24 19:37:58 -0300

    config struct

--- piscou.h
+++ piscou.h
@@ -7,9 +7,23 @@ typedef struct Command {
 } Command;
 
 Command commands[] = {
-    {.mime = "text/*",  .args = {"bat", "-p", "main.c"}},
-    {.mime = "image/*", .args = {"echo", "image", "%piscou-filename%"}},
-    {.mime = "video/*", .args = {"vid.sh", "%piscou-filename"}},
+{"text/.*", {"bat", "-p", "--pager=never", "--color=always", "%piscou-filename%"}},
+{"inode/directory", {"ls", "-1A", "--color", "%piscou-filename%"}},
+{"ms(word|-excel|-power)", {"printf", "💩💩💩💩💩💩💩💩💩\n%s\n💩💩💩💩💩💩💩💩💩", "%piscou-filename%"}},
+{"opendoc.+spreadsheet", {"ods.sh", "%piscou-filename%"}},
+{"office.+pres", {"ppt.sh", "%piscou-filename%", "%piscou-extra0%"}},
+{"opendocument", {"odt2txt", "%piscou-filename%"}},
+{"application/pdf", {"pdf.sh", "%piscou-filename%", "%piscou-extra0%", "%piscou-extra1%", "%piscou-extra2%", "%piscou-extra3%"}},
+{"application/csv", {"column", "-t", "-s", ",", "%piscou-filename%"}},
+{"application/json", {"head -n 40", "%piscou-filename%"}},
+{"fpath .+\\.ff$", {"stiv", "%piscou-filename%", "%piscou-extra0%", "%piscou-extra1%", "%piscou-extra2%", "%piscou-extra3%"}},
+{"fpath .+\\.[1-9]$", {"man", "%piscou-filename%"}},
+{"image/.*dwg", {"stat", "%piscou-filename%"}},
+{"image/.*xml", {"head -n 40", "%piscou-filename%"}},
+{"image/.*", {"stiv", "%piscou-filename%", "%piscou-extra0%", "%piscou-extra1%", "%piscou-extra2%", "%piscou-extra3%"}},
+{"audio/.*", {"vid.sh", "%piscou-filename%", "%piscou-extra0%", "%piscou-extra1%", "%piscou-extra2%", "%piscou-extra3%"}},
+{"video/.*", {"vid.sh", "%piscou-filename%", "%piscou-extra0%", "%piscou-extra1%", "%piscou-extra2%", "%piscou-extra3%"}},
+{".+", {"file", "--mime-type", "%piscou-filename%"}}
 };
 
 #endif /* piscou_h */
