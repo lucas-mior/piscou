9b8d23185119534afc506ebbd8e86b292ffc3d6b
lucas-mior <lucas.mior@tutamail.com>
2024-08-21 14:29:11 -0300

    Revert "not working: pass bytes to exec via stdin"
    This reverts commit b1576aa26ba695125215692275b12dc820036d1f.

--- config.h
+++ config.h
@@ -3,7 +3,6 @@
 #define MAX_ARGS 16
 #define MAX_EXTRAS 10
 #define MAX_ARGUMENT_LENGTH 256
-#define REGEX_BYTES "%piscou-bytes%"
 #define REGEX_FILENAME "%piscou-filename%"
 #define REGEX_EXTRAS "^%piscou-extra([0-9])%$"
 #define REGEX_EXTRAS_MORE "%piscou-extra([0-9])%"
@@ -53,5 +52,5 @@ static const Rule rules[] = {
 {{"video/.*",                NULL}, {"vid.sh", "%piscou-filename%", "%piscou-extra0%", "%piscou-extra1%", "%piscou-extra2%", "%piscou-extra3%"}},
 {{"appl[^/]+/x-matroska",    NULL}, {"vid.sh", "%piscou-filename%", "%piscou-extra0%", "%piscou-extra1%", "%piscou-extra2%", "%piscou-extra3%"}},
 {{"appl[^/]+/x-kicad-.*",    NULL}, {"kicad.sh", "%piscou-filename%", "%piscou-extra0%", "%piscou-extra1%", "%piscou-extra2%", "%piscou-extra3%"}},
-{{"text/.+",                 NULL}, {"bat", "-p", "--pager=never", "--color=always", "%piscou-bytes%"}},
+{{"text/.+",                 NULL}, {"bat", "-p", "--pager=never", "--color=always", "%piscou-filename%"}},
 };
