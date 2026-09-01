d4e890abd227790de9248c294890f6bd5ab0a529
lucas-mior <lucas.mior@tutamail.com>
2026-09-01 10:12:16 -0300

    prepare to fix warnings

--- build.sh
+++ build.sh
@@ -53,8 +53,7 @@ CPPFLAGS="$CPPFLAGS -Icbase"
 
 CFLAGS="$CFLAGS -std=c11"
 CFLAGS="$CFLAGS -Wfatal-errors"
-# CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
-CFLAGS="$CFLAGS -Wno-unused-function"
+CFLAGS="$CFLAGS -Werror"  # Only uncomment occasionally, keep this line
 CFLAGS="$CFLAGS $PTHREAD_CFLAGS"
 
 if [ "$CC" = "clang" ] || [ "$CC" = "zig cc" ]; then
